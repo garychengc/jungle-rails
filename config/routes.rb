ae283617ce3dc97c4813547c71477455b5f9121e
Gary Chen <garychen.whc@gmail.com>
2019-10-17 23:05:14 +0000

    added bcrypt for login, register

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   resources :about, only: [:index]
+  
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  get "/register" => "users#new"
+  post "/users" => "users#create"
 
   namespace :admin do
     root to: 'dashboard#show'
